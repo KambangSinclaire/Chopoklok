4c5a13f25d8761c9a83c8761bc5f904895f59e06
Joram <abayojoram696@gmail.com>
2023-01-16 15:07:05 +0100

    Class adjustments in icons

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -49,7 +49,7 @@ const Footer = () => {
       <div className="newsletter">
         <input type="email" placeholder='Enter your email' />
         <span>
-          <i class="ri-send-plane-line"></i>
+          <i className="ri-send-plane-line"></i>
         </span>
       </div>
     </Col>
@@ -67,25 +67,25 @@ const Footer = () => {
       <span>
         {""}
       <Link to="">
-        <i class="ri-facebook-line"></i>
+        <i className="ri-facebook-line"></i>
       </Link>
       </span>
       <span>
         {""}
       <Link to="">
-        <i class="ri-github-line"></i>
+        <i className="ri-github-line"></i>
       </Link>
       </span>
       <span>
         {""}
       <Link to="">
-        <i class="ri-youtube-line"></i>
+        <i className="ri-youtube-line"></i>
       </Link>
       </span>
       <span>
         {""}
       <Link to="">
-        <i class="ri-linkedin-line"></i>
+        <i className="ri-linkedin-line"></i>
       </Link>{""}
       </span>
     </div>
